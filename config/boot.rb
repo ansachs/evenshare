c5c8ea12c1a4a5a48df4598d3c48e8302217ab8c
ansachs <ansachs@gmail.com>
2017-11-23 16:54:54 -0600

    heroku is failing to compile'

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 #ansachs added this line to force development
-ENV['RAILS_ENV'] = 'development'
+# ENV['RAILS_ENV'] = 'development'
