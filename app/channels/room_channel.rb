6ca90554506a34bff1698c2f2d08103a9de1a8a2
ansachs <ansachs@gmail.com>
2017-11-25 23:44:29 -0600

    added pry remote

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,14 +2,13 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     # binding.pry 
-    
-
     stream_from "room"
   end
 
-  # def unsubscribed
-  #   # Any cleanup needed when channel is unsubscribed
-  # end
+  def unsubscribed
+    stop_all_streams
+  end
+
 
   # def speak(data)
   #   Chat.create!(statement: data['message'], user_id: 1, concert_id: 1)
