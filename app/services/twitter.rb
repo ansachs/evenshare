9083459982ef0b57ee32fe907e14e25e70f8c3fa
ansachs <ansachs@gmail.com>
2017-11-28 20:52:32 -0600

    adding components for twitter

--- app/services/twitter.rb
+++ app/services/twitter.rb
@@ -1,11 +1,17 @@
 # require 'faraday'
 # require 'faraday_middleware'
+require 'twitter'
 
 
 
-# client = Twitter::REST::Client.new do |config|
-#   config.consumer_key        = "YOUR_CONSUMER_KEY"
-#   config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-#   config.access_token        = "YOUR_ACCESS_TOKEN"
-#   config.access_token_secret = "YOUR_ACCESS_SECRET"
-# end
+client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "boX4lrChqQi3LB9fJYSfYnzbT"
+  config.consumer_secret     = "kj2NTBpt6IxZNqtW1wWz7VCcpr7uYwJWzPKCppS5ZwbSaHhijq"
+  config.access_token        = "935516830508216321-AiV47Wioo6Uw97srLznYtUG5lBk7yri"
+  config.access_token_secret = "37z2jQLOfZXczLGGWMfp7VKHv0keJjs2YyCdd2885sVB8"
+end
+
+topics = ["coffee", "tea"]
+client.filter(track: "justinbieber") do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
