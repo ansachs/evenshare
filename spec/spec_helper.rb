8edcabcc40571f291070db9efe242f05335c0c72
ansachs <ansachs@gmail.com>
2017-12-02 15:08:09 -0600

    added tests for two services

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  # require_relative './support/vcr'
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
@@ -87,7 +88,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = :random
-
+  
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
   # test failures related to randomization by passing the same `--seed` value
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,4 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/support/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.default_cassette_options = { :record => :new_episodes, :re_record_interval => 1.days }
 end
