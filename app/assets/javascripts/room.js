5c8669ebfb2d8be6287b0c00f66b2a046ff8999b
ansachs <ansachs@gmail.com>
2017-12-03 22:47:55 -0600

    pushing to heroku

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,28 +1,3 @@
-// $(document).on 'keypress', '[data-behavior~=room_speaker]', (event) -> 
-//       console.log(event)
-//       event.preventDefault() 
-//       if event.keyCode is 13 # return/enter = send
 
-//         App.room.speak event.target.value 
-//         event.target.value = ''
-//         console.log('wtf') 
-
-// $(document).on('turbolinks:load', function() {
-//   submitNewMessage();
-// });
-
-// function submitNewMessage(){
-  // $('document').keydown(function(event) {
-  //   if (event.keyCode == 13) {
-  //       App.room.speak event.target.value 
-  //       event.target.value = ''
-  //    }
-  // });
-
-// $(document).on('turbolinks:load', function() {
-//   // scrollBottom();  
-
-
-// });
 
 
