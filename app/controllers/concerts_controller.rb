c24cda38d2d1247e4b642fec8f02ef0da61f13d4
ansachs <ansachs@gmail.com>
2018-02-21 20:04:03 -0600

    added link page

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,9 +1,11 @@
 class ConcertsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     concert_object = LoadConcerts.new
     @concerts = concert_object.concert_logic(params)
+  end
+
+  def about
   end
 
 end
