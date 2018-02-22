266a44b4484724f3deb68f7952cfbe15b8e55c06
ansachs <ansachs@gmail.com>
2018-02-21 21:20:06 -0600

    fixed date

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -3,6 +3,11 @@ class ConcertsController < ApplicationController
   def index
     concert_object = LoadConcerts.new
     @concerts = concert_object.concert_logic(params)
+    if params[:date] == nil 
+      @date = Time.now.strftime("%Y-%m-%d")
+    else
+      @date = params[:date]
+    end
   end
 
   def about
